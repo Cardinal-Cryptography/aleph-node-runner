ea38098aece3a86387789efdd4216323b52c8463
piotrMocz <piotr.moczurad@gmail.com>
2022-10-12 14:48:37 +0200

    Make the stash account optional for validators as well

--- run_node.sh
+++ run_node.sh
@@ -79,11 +79,6 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [[ -z "${ARCHIVIST}" ]]
-then
-    STASH_ACCOUNT=${STASH_ACCOUNT:?'Please specify your stash account_id.'}
-fi
-
 ALEPH_VERSION=$(cat env/version)
 
 mkdir -p ${DB_SNAPSHOT_PATH}
@@ -145,6 +140,12 @@ echo 'Performing session key checks...'
 if [[ -n "${ARCHIVIST}" ]]
 then
     echo 'Node run as archivist: no need to check session keys.'
+    exit 0
+fi
+
+if [[ -z "${STASH_ACCOUNT}" ]]
+then
+    echo "Stash account not provided: skipping the session keys check."
     exit 0
 fi
 
