d5422dfad45d55afb1f65a52dc460d2f484087f6
piotrMocz <piotr.moczurad@gmail.com>
2023-11-13 13:41:51 +0100

    Allow manually overriding the version to run. Remove session key checks

--- run_node.sh
+++ run_node.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+# for coloring the output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BGREEN='\033[1;32m'
+NC='\033[0m'
+
 Help()
 {
     cat <<EOF
@@ -12,16 +18,16 @@ options:
 archivist         Run the node as an archivist (the default is to run as a validator).
 ip                The public IP of your node.
 dns               The public DNS of your node.
-stash_account     Stash account of your validator: optional but recommended, if you're re-running the script.
 n | name          Set the node's name.
 d | data_dir      Specify the directory where all the chain data will be stored (default: ~/.alephzero).
 mainnet           Join the mainnet (by default the script will join testnet).
+version           Manually override the version to run (accepts a git tag or a short git commit hash).
 build_only        Do not run after the setup.
 sync_from_genesis Perform a full sync instead of downloading the backup.
 help              Print this help.
 
 Example usage:
-$0 --name my-aleph-node --stash_account 5CeeD3MGHCvZecJkvfJVzYvYkoPtw9pTVvskutXAUtZtjcYa --ip 123.123.123.123
+$0 --name my-aleph-node --ip 123.123.123.123
 
 EOF
 }
@@ -31,6 +37,12 @@ get_version () {
     # The version that is returned by the extrinsic looks like: 0.11.4-ae34eb4213
     # so we take the hash part and use it to identify docker images and commits in the repo.
 
+    if [ -n "${VERSION}" ]
+    then
+        ALEPH_VERSION="${VERSION}"
+        return
+    fi
+
     if  [ -n "${MAINNET}" ]
     then
         VERSION_RPC_URL="https://rpc.azero.dev"
@@ -38,10 +50,21 @@ get_version () {
         VERSION_RPC_URL="https://rpc.test.azero.dev"
     fi
 
+    set +e    # we want to inspect the status of the curl command
+
     # call the RPC endpoint to get the version
-    VERSION_INFO=$(curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_version"}' ${VERSION_RPC_URL})
+    VERSION_INFO=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "system_version"}' ${VERSION_RPC_URL})
+    if [ 0 -ne $? ]
+    then
+        echo -e "${RED}Failed to reach the version endpoint.${NC}"
+        echo "Please re-run the script, passing the appropriate version with the --version option."
+        exit 2
+    fi
+
+    set -e
+
     # use jq to parse the resulting json, get the value of the version field and strip quotes
-    VERSION_INFO=$(echo "${VERSION_INFO}" | docker run -i "${JQ_IMAGE}" '.result' | tr -d '"')
+    VERSION_INFO=$(echo "${VERSION_INFO}" | docker run --quiet -i "${JQ_IMAGE}" '.result' | tr -d '"')
     # only return the commit hash that comes after a '-'
     ALEPH_VERSION=${VERSION_INFO##*-}
 }
@@ -60,13 +83,14 @@ check_default_dir () {
 
         if [[ "$CONT" == 'n' ]]
         then
-            echo "Please re-run the script, supplying the '--data_dir' argument, exiting."
+            echo -e "${RED}Please re-run the script, supplying the '--data_dir' argument, exiting.${NC}"
             exit 0
         fi
 
         echo "Moving the data from ${DB_SNAPSHOT_PATH} into ${HOST_BASE_PATH}/${DB_SNAPSHOT_PATH}..."
         mkdir -p "${HOST_BASE_PATH}"/${DB_SNAPSHOT_PATH}
         mv ${DB_SNAPSHOT_PATH}/* "${HOST_BASE_PATH}"/${DB_SNAPSHOT_PATH}
+        echo -e "${GREEN}Finished moving the data.${NC}"
     fi
 }
 
@@ -79,26 +103,26 @@ get_snapshot () {
     if [ ! -d "${DB_SNAPSHOT_PATH}/db/full" ] && [ -z "$SYNC" ]
     then
         echo "Downloading the snapshot..."
-        pushd "${DB_SNAPSHOT_PATH}"
-        wget -O ${DB_SNAPSHOT_FILE} ${DB_SNAPSHOT_URL}
+        pushd "${DB_SNAPSHOT_PATH}" > /dev/null
+        wget -q -O ${DB_SNAPSHOT_FILE} ${DB_SNAPSHOT_URL}
         tar xvzf ${DB_SNAPSHOT_FILE}
         rm ${DB_SNAPSHOT_FILE}
-        popd
+        popd > /dev/null
     fi
 }
 
 get_chainspec () {
     # For testnet and mainnet we get the chainspec from the repo (with the commit identified by the hash we get from System::version RPC call)
     echo "Downloading the chainspec..."
-    pushd "${HOST_BASE_PATH}"
-    wget -O ${CHAINSPEC_FILE} https://raw.githubusercontent.com/Cardinal-Cryptography/aleph-node/"${ALEPH_VERSION}"/bin/node/src/resources/${CHAINSPEC_FILE}
-    popd
+    pushd "${HOST_BASE_PATH}" > /dev/null
+    wget -q -O ${CHAINSPEC_FILE} https://raw.githubusercontent.com/Cardinal-Cryptography/aleph-node/"${ALEPH_VERSION}"/bin/node/src/resources/${CHAINSPEC_FILE}
+    popd > /dev/null
 }
 
 get_docker_image () {
     echo "Pulling docker image..."
     ALEPH_IMAGE=public.ecr.aws/p6e8q1z1/aleph-node:${ALEPH_VERSION:0:7}
-    docker pull "${ALEPH_IMAGE}"
+    docker pull --quiet "${ALEPH_IMAGE}"
 }
 
 run_validator () {
@@ -126,8 +150,8 @@ run_validator () {
         PUBLIC_VALIDATOR_ADDRESS="${PUBLIC_IP}:${PROXY_VALIDATOR_PORT}"
     fi
 
-    echo "Running with public P2P address: ${PUBLIC_ADDR}"
-    echo "And validator address: ${PUBLIC_VALIDATOR_ADDRESS}"
+    echo -e "Running with public P2P address: ${GREEN}${PUBLIC_ADDR}${NC}"
+    echo -e "And validator address: ${GREEN}${PUBLIC_VALIDATOR_ADDRESS}${NC}"
 
     PORT_MAP="${PORT}:${PORT}"
     VALIDATOR_PORT_MAP="${VALIDATOR_PORT}":"${VALIDATOR_PORT}"
@@ -170,7 +194,7 @@ run_archivist () {
         PUBLIC_ADDR="/ip4/${PUBLIC_IP}/tcp/${PROXY_PORT}"
     fi
 
-    echo "Running with public P2P address: ${PUBLIC_ADDR}"
+    echo -e "Running with public P2P address: ${GREEN}${PUBLIC_ADDR}${NC}"
 
     PORT_MAP="${PORT}:${PORT}"
 
@@ -224,18 +248,17 @@ while [[ $# -gt 0 ]]; do
             DB_SNAPSHOT_PATH="chains/mainnet/"
             CHAINSPEC_FILE="mainnet_chainspec.json"
             DB_SNAPSHOT_URL="${MAINNET_DB_SNAPSHOT_URL}"
-            CLIAIN_ENDPOINT='wss://ws.azero.dev:443'
             MAINNET=true
             shift;;
+        --version) # Run a specific version of the binary
+            VERSION="$2"
+            shift 2;;
         --build_only)
             BUILD_ONLY=true
             shift;;
         --sync_from_genesis)
             SYNC=true
             shift;;
-        --stash_account)
-            STASH_ACCOUNT=$2
-            shift 2;;
         --proxy_port)
             PROXY_PORT=$2
             shift 2;;
@@ -254,14 +277,14 @@ done
 
 if [ -z "${PUBLIC_IP}" ] && [ -z "${PUBLIC_DNS}" ]
 then
-    echo "You need to provide either a public ip address of your node (--ip) or a public dns address of your node (--dns)."
-    exit 2
+    echo -e "${RED}You need to provide either a public ip address of your node (--ip) or a public dns address of your node (--dns).${NC}"
+    exit 3
 fi
 
 ### Figure out the version to run
 # Check the external jq image
 JQ_IMAGE='stedolan/jq:latest'
-docker image inspect "${JQ_IMAGE}" > /dev/null && echo "JQ image check: OK"
+docker image inspect "${JQ_IMAGE}" > /dev/null && echo -e "JQ image check: ${GREEN}OK${NC}"
 
 get_version
 
@@ -275,6 +298,7 @@ get_docker_image
 
 if [ -z "$BUILD_ONLY" ]
 then
+    echo ""
     echo "Running the node..."
 
     # remove the container if it exists
@@ -291,93 +315,10 @@ then
     fi
 fi
 
-if [[ -n "${ARCHIVIST}" ]]
-then
-    echo 'Node run as archivist: no need to check session keys.'
-    exit 0
-fi
-
 echo ""
-echo 'Performing session key checks...'
-
-if [[ -z "${STASH_ACCOUNT}" ]]
-then
-    echo "Stash account not provided. This is ok if you're running the script for the first time but recommended for subsequent runs."
-    read -p "Are you sure you want to skip the session keys check? [y/N]" -r -n 1
-    echo ""
-
-    if [[ "${REPLY}" =~ ^[Yy] ]]
-    then
-        echo "Skipping the session keys check."
-        exit 0
-    fi
-
-    read -p "Please provide your stash account: " -r STASH_ACCOUNT
-fi
-
-## Now we will attempt to check validator's session keys
-CLIAIN_IMAGE='public.ecr.aws/p6e8q1z1/cliain:latest'
-CLIAIN_ENDPOINT=${CLIAIN_ENDPOINT:-'wss://ws.test.azero.dev:443'}
-
-# Pull cliain from ecr
-docker pull "${CLIAIN_IMAGE}"
-
-# Try to retrieve set session keys from chain's storage
-CLIAIN_NAME="cliain-$(xxd -l "16" -p /dev/urandom | tr -d " \n" ; echo)"
-if ! SESSION_KEYS_JSON=$(docker run --name="${CLIAIN_NAME}" "${CLIAIN_IMAGE}" --node "${CLIAIN_ENDPOINT}" \
-    next-session-keys --account-id "${STASH_ACCOUNT}" 2> /dev/null);
-then
-    # This should not happen even if the keys are not set
-    echo "Cliain failed when trying to retrieve keys for this stash account. Logs:"
-    docker logs "${CLIAIN_NAME}"
-    docker rm "${CLIAIN_NAME}"
-    exit 0
-fi
-
-docker rm "${CLIAIN_NAME}"
-
-# Check if there are any session keys set for the specified stash account
-if [[ -n "${SESSION_KEYS_JSON}" ]]
-then
-    # Check the external jq image
-    docker image inspect "${JQ_IMAGE}" > /dev/null && echo "JQ image check: OK"
-    # Read keys from the JSON
-    AURA_KEY=$(echo "${SESSION_KEYS_JSON}" | docker run -i "${JQ_IMAGE}" '.aura' | tr -d '"')
-    ALEPH_KEY=$(echo "${SESSION_KEYS_JSON}" | docker run -i "${JQ_IMAGE}" '.aleph' | tr -d '"')
-
-    # Format keys into string notation
-    ALEPH_KEY_TRUNCATED="${ALEPH_KEY#"0x"}"
-    SESSION_KEYS_STRING="${AURA_KEY}${ALEPH_KEY_TRUNCATED}"
-
-    # Perform an RPC call to the local node to check whether it has access to the keys
-    HAS_KEYS_RESULT_JSON=$(curl -H "Content-Type: application/json" \
-        -d '{"id":1, "jsonrpc":"2.0", "method": "author_hasSessionKeys",
-            "params":["'"${SESSION_KEYS_STRING}"'"]}' http://127.0.0.1:"${RPC_PORT}" 2> /dev/null)
-
-    HAS_KEYS_RESULT=$(echo "${HAS_KEYS_RESULT_JSON}" | docker run -i "${JQ_IMAGE}" '.result')
-
-    if [[ "${HAS_KEYS_RESULT}" == false ]]
-    then
-        # If the keys are not present, then we stop the node and print the message
-        # (the node would not be able to validate properly anyway)
-
-        RED='\033[0;31m'
-        NC='\033[0m'
-
-        >&2 echo -e "${RED}"
-        >&2 echo "Session keys are set for this stash account, but it seems like you do not have access to them."
-        >&2 echo "You might want to generate new keys and set them for your stash account."
-        >&2 echo "Stopping the node..."
-        >&2 echo -e "${NC}"
-
-        # Stop the node
-        docker stop "${NAME}"
-        exit 1
-    fi
-else
-    echo ""
-    echo "Specified account do not have any session keys set."
-    echo "Check https://docs.alephzero.org/aleph-zero/validate/troubleshooting#generating-your-session-keys for details"
-fi
-
-exit 0
+echo -e "${BGREEN}Launched the node!${NC}"
+echo ""
+echo "Please check if the node is running correctly by first running:"
+echo -e "  ${GREEN}docker ps${NC}"
+echo "And then, if the status is 'Up', inspect the logs by running:"
+echo -e "  ${GREEN}docker logs ${NAME}${NC}"
